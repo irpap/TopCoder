181dd17cd73e0bb300daef6caa2e28f2e5e64593
Irene Papakonstantinou <eirhnh@gmail.com>
2012-07-03 14:18:02 +0100

    It turns out I read the problem statement wrong. Starting over, doing a backtracking on the length. Later I can memoize and find the number of fragments instead of just the longest length

--- CMajor.java
+++ CMajor.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 /**
  * SRM 289 DIV 1
  */
@@ -20,72 +22,55 @@ public class CMajor {
     int[] keyIndex = {
             0, 2, 3, 5, 7, 8, 10
     };
+    private static final char INVALID = (char) -1;
 
+    public int longestMelodyLength(LinkedList<String> fragments, char startingKey) {
+        //try to include and to not include, return biggest
+        if (fragments.isEmpty()) return 0;
+        if (startingKey == INVALID) return 0;
 
-    public int getLongest(String[] fragments) {
-        final int[][] DP = new int[fragments.length][WHITE_KEYS.length];
+        //include the first fragment
+        final String head = fragments.pollFirst();
+        final char endPoint = endOfFragment(startingKey, head);
+        int lengthIncluding = keysInFragment(head) + longestMelodyLength(fragments, endPoint);
 
-        for (int i = 0; i < fragments.length; i++) {
-            int[] lengthsForWhite = new int[WHITE_KEYS.length];
-            for (int j = 0; j < WHITE_KEYS.length; j++) {
-                lengthsForWhite[j] = isValidStart(WHITE_KEYS[j], fragments[i]) ? keysInFragment(fragments[i]) : 0;
-            }
-            DP[i] = lengthsForWhite;
-        }
-        final int[][] melodyLengths = deepCopy(DP);
+        //don't include the first fragment
+        int lengthExcluding = longestMelodyLength(fragments, startingKey);
 
-        for (int i = 1; i < melodyLengths.length; i++) {
-            for (int j = 0; j < melodyLengths[i].length; j++) {
-                int prevFragmentLength = melodyLengths[i - 1][j];
-                melodyLengths[i][j] = prevFragmentLength + melodyLengths[i][j];
-            }
-        }
-        //find max Element the last row, and trace back the number of non zeros.
-        return numberOfFragmentsInMaxMelody(DP, melodyLengths);
-    }
-
-    private int[][] deepCopy(int[][] DP) {
-        int[][] melodyLenghts = new int[DP.length][WHITE_KEYS.length];
-        for (int i = 0; i < DP.length; i++) {
-            for (int j = 0; j < DP[i].length; j++) {
-                melodyLenghts[i][j] = DP[i][j];
-            }
-        }
-        return melodyLenghts;
-    }
-
-    private int numberOfFragmentsInMaxMelody(int[][] dp, int[][] melodyLengths) {
-        int max = melodyLengths[melodyLengths.length - 1][0];
-        int maxIndex = 0;
-        for (int i = 0; i < melodyLengths[melodyLengths.length - 1].length; i++) {
-            if (melodyLengths[melodyLengths.length - 1][i] > max) {
-                max = melodyLengths[melodyLengths.length - 1][i];
-                maxIndex = i;
-            }
-        }
-
-        int countNoZeros = 0;
-        for (int i = 0; i < dp.length; i++) {
-            if (dp[i][maxIndex] != 0) countNoZeros++;
-        }
-        return countNoZeros;
+        return Math.max(lengthIncluding, lengthExcluding);
     }
 
     private int keysInFragment(String fragment) {
         return fragment.length() - fragment.replaceAll(" ", "").length() + 1;
     }
 
-    private boolean isValidStart(char c, String fragment) {
-
+    /**
+     * If this fragment can be played with this starting key, it returns the end key. If any black key is encountered returns INVALID.
+     */
+    private char endOfFragment(char c, String fragment) {
+        System.out.println("starting char: " + c);
         int start = keyIndex[c - 'A'];
         final String[] jumps = fragment.split(" ");
         for (String j : jumps) {
             int jump = Integer.parseInt(j);
             int next = (Math.abs(start + jump)) % ALL_KEYS.length;
-            if (!ALL_KEYS[next]) return false;
+            if (!ALL_KEYS[next]) {
+                System.out.println("returning INVALID");
+                return INVALID;
+            }
             start = next;
         }
-        return true;
+        char result = ALL_KEYS[start] ? whiteKeyForIndex(start) : INVALID;
 
+        System.out.println("returning: " + result);
+        return result;
+
+    }
+
+    private char whiteKeyForIndex(int whiteKey) {
+        for (int i = 0; i < keyIndex.length; i++) {
+            if (whiteKey == keyIndex[i]) return (char) (i + 'A');
+        }
+        return INVALID;
     }
 }
