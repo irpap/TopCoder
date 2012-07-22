96f7e962847dd36810d61db8a9c16c35262d4f9d
Irene <eirhnh@gmail.com>
2012-07-22 13:48:53 +0100

    refactoring, moving these arrays into fields in the node class

--- BombMan.java
+++ BombMan.java
@@ -9,52 +9,42 @@ import java.util.PriorityQueue;
 public class BombMan {
     private static int UNDISCOVERED = 0;
     private static int DISCOVERED = 1;
-
-    private char[][] m;
-    private int[][] timeSpent;
-    private int[][] state;
-    private int[][] bombsLeft;
+    private Node[][] nodes;
 
     public int shortestPath(String[] maze, int bombs) {
-        m = new char[maze.length][maze[0].length()];
-        state = new int[maze.length][maze[0].length()];
-        bombsLeft = new int[maze.length][maze[0].length()];
-        timeSpent = new int[maze.length][maze[0].length()];
+        nodes = new Node[maze.length][maze[0].length()];
         for (int i = 0; i < maze.length; i++) {
             for (int j = 0; j < maze[i].length(); j++) {
-                m[i][j] = maze[i].charAt(j);
+                nodes[i][j] = new Node(new Point(i, j), maze[i].charAt(j));
             }
         }
 
-        Point start = findStart(m);
+        Point start = findStart(nodes);
 
-        PriorityQueue<Node> q = new PriorityQueue<Node>(m.length * m[0].length, new Comparator<Node>() {
+        PriorityQueue<Node> q = new PriorityQueue<Node>(nodes.length * nodes[0].length, new Comparator<Node>() {
             public int compare(Node o1, Node o2) {
-                return timeSpent[o1.p.i][o1.p.j] - timeSpent[o2.p.j][o2.p.j];
+                return nodes[o1.p.i][o1.p.j].timeSpent - nodes[o2.p.j][o2.p.j].timeSpent;
             }
         });
 
-        q.add(new Node(start));
-        state[start.i][start.j] = DISCOVERED;
-        bombsLeft[start.i][start.j] = bombs;
+        q.add(new Node(start, nodes[start.i][start.j].m));
+        nodes[start.i][start.j].state = DISCOVERED;
+        nodes[start.i][start.j].bombsLeft = bombs;
         while (!q.isEmpty()) {
             Node top = q.poll();
-            System.out.print("\nvisiting (" + top.p.i + "," + top.p.j + ")" + m[top.p.i][top.p.j]);
-            System.out.println(" time passed: " + timeSpent[top.p.i][top.p.j] + ", bombs left: " + bombsLeft[top.p.i][top.p.j]);
-            int topDistance = timeSpent[top.p.i][top.p.j];
+            int topDistance = nodes[top.p.i][top.p.j].timeSpent;
             if (isExit(top)) return topDistance;
             for (Point neighbor : neighboringPoints(top.p)) {
-                System.out.println("\tcoming from  (" + top.p.i + ", " + top.p.j + ") visiting (" + neighbor.i + "," + neighbor.j + ")");
-                if (state[neighbor.i][neighbor.j] == UNDISCOVERED) {
-                    state[neighbor.i][neighbor.j] = DISCOVERED;
-                    Node neighborNode = new Node(neighbor);
+                if (nodes[neighbor.i][neighbor.j].state == UNDISCOVERED) {
+                    nodes[neighbor.i][neighbor.j].state = DISCOVERED;
+                    Node neighborNode = new Node(neighbor, nodes[neighbor.i][neighbor.j].m);
                     q.add(neighborNode);
-                    if (m[neighbor.i][neighbor.j] == '#' && bombsLeft[top.p.i][top.p.j] > 0) {
-                        bombsLeft[neighbor.i][neighbor.j] = bombsLeft[top.p.i][top.p.j] - 1;
-                        timeSpent[neighbor.i][neighbor.j] = topDistance + 3;
+                    if (nodes[neighbor.i][neighbor.j].m == '#' && nodes[top.p.i][top.p.j].bombsLeft > 0) {
+                        nodes[neighbor.i][neighbor.j].bombsLeft = nodes[top.p.i][top.p.j].bombsLeft - 1;
+                        nodes[neighbor.i][neighbor.j].timeSpent = topDistance + 3;
                     } else {
-                        timeSpent[neighbor.i][neighbor.j] = topDistance + 1;
-                        bombsLeft[neighbor.i][neighbor.j] = bombsLeft[top.p.i][top.p.j];
+                        nodes[neighbor.i][neighbor.j].timeSpent = topDistance + 1;
+                        nodes[neighbor.i][neighbor.j].bombsLeft = nodes[top.p.i][top.p.j].bombsLeft;
                     }
                 }
             }
@@ -66,29 +56,35 @@ public class BombMan {
         LinkedList<Point> neighbors = new LinkedList<Point>();
         if (p.i - 1 >= 0) neighbors.add(new Point(p.i - 1, p.j));
         if (p.j - 1 >= 0) neighbors.add(new Point(p.i, p.j - 1));
-        if (p.i + 1 < m.length) neighbors.add(new Point(p.i + 1, p.j));
-        if (p.j + 1 < m.length) neighbors.add(new Point(p.i, p.j + 1));
+        if (p.i + 1 < nodes.length) neighbors.add(new Point(p.i + 1, p.j));
+        if (p.j + 1 < nodes.length) neighbors.add(new Point(p.i, p.j + 1));
         return neighbors;
     }
 
     private boolean isExit(Node node) {
-        return m[node.p.i][node.p.j] == 'E';
+        return nodes[node.p.i][node.p.j].m == 'E';
     }
 
-    private Point findStart(char[][] m) {
+    private Point findStart(Node[][] m) {
         for (int i = 0; i < m.length; i++) {
             for (int j = 0; j < m[i].length; j++) {
-                if (m[i][j] == 'B') return new Point(i, j);
+                if (nodes[i][j].m == 'B') return new Point(i, j);
             }
         }
         return null;
     }
 
     private static class Node {
+        private char m;
+        private int timeSpent;
+        private int state;
+        private int bombsLeft;
+
         public Point p;
 
-        public Node(Point point) {
+        public Node(Point point, char m) {
             this.p = point;
+            this.m = m;
         }
     }
 
