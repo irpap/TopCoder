dbae11ac0a02f1adf094b992acf7db1091b45552
Irene <eirhnh@gmail.com>
2012-08-01 14:32:02 +0100

    kingdom and trees in progress. flower garden passes the examples

--- FlowerGarden.java
+++ FlowerGarden.java
@@ -1,22 +1,55 @@
-/*
- * -----------------------------------------------------------------------
- *
- * QATARLYST LIMITED
- *
- * -----------------------------------------------------------------------
- *
- * (C) Copyright 2012 Qatarlyst Limited. All rights reserved.
- *
- * NOTICE:  All information contained herein or attendant hereto is,
- *          and remains, the property of Qatarlyst Limited.  Many of the
- *          intellectual and technical concepts contained herein are
- *          proprietary to Qatarlyst Limited. Any dissemination of this
- *          information or reproduction of this material is strictly
- *          forbidden unless prior written permission is obtained
- *          from Qatarlyst Limited.
- *
- * -----------------------------------------------------------------------
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * 2004 TCCC Online Round 1 - Division I, Level Two
  */
 
 public class FlowerGarden {
+
+
+    private Flower[] flowers;
+
+    public int[] getOrdering(int[] height, int[] bloom, int[] wilt) {
+        flowers = new Flower[height.length];
+        for (int i = 0; i < height.length; i++) {
+            flowers[i] = new Flower(height[i], bloom[i], wilt[i]);
+        }
+
+
+        Arrays.sort(flowers, new Comparator<Flower>() {
+            public int compare(Flower f1, Flower f2) {
+                if (livesOverlap(f1, f2)) {
+                    f1.born = f2.born = Math.min(f1.born, f2.born);
+                    f1.die = f2.die = Math.max(f1.die, f2.die);
+                    return f1.height - f2.height;
+
+                } else return (f2.height - f1.height);
+            }
+        });
+
+        int[] order = new int[flowers.length];
+
+        for (int i = 0; i < flowers.length; i++) {
+            order[i] = flowers[i].height;
+        }
+        return order;
+    }
+
+    private boolean livesOverlap(Flower f1, Flower f2) {
+        return Math.min(f1.die, f2.die) >= Math.max(f1.born, f2.born);
+    }
+
+
+    private static class Flower {
+        int height;
+        int born;
+        int die;
+
+        private Flower(int height, int born, int die) {
+            this.height = height;
+            this.born = born;
+            this.die = die;
+        }
+    }
 }
--- KingdomAndTrees.java
+++ KingdomAndTrees.java
@@ -1,2 +1,37 @@
 public class KingdomAndTrees {
+
+    private int[] heights;
+
+    public int minLevel(int[] heights) {
+        this.heights = heights;
+        int low = 0;
+        int high = Math.max(heights.length, range(heights));
+        return search(low, high);
+    }
+
+    private int range(int[] heights) {
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for (int h : heights) {
+            if (h < min) min = h;
+            if (h > max) max = h;
+        }
+        return max - min;
+    }
+
+    int search(int low, int high) {
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (isEnough(mid, heights)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;    // the least level that is enough
+    }
+
+    private boolean isEnough(int level, int[] heights) {
+        return true;
+    }
 }
