17725e07d7a818d8b64b5c97eb119ba793afd2a3
Irene <eirhnh@gmail.com>
2012-08-13 21:21:12 +0100

    using ford fulkerson to calculate a maximum bipartite matching. still has a bug somewhere

--- PointyWizardHats.java
+++ PointyWizardHats.java
@@ -6,56 +6,53 @@ import java.util.Arrays;
  * Single Round Match 549 Round 1 - Division II, Level Two
  */
 public class PointyWizardHats {
-    public int getNumHats(int[] topHeight, int[] topRadius, int[] bottomHeight, int[] bottomRadius) {
-        Hat[] topHats = new Hat[topHeight.length];
-        Hat[] bottomHats = new Hat[bottomHeight.length];
+    //size of tophats - Left side
+    private int m;
+    //size of bottomhats - Right side
+    private int n;
+    private boolean[][] graph;
+    boolean seen[];
+    //What left vertex i is matched to (or -1 if unmatched)
+    int matchL[];
+    //What right vertex j is matched to (or -1 if unmatched)
+    int matchR[];
 
-        for (int i = 0; i < topHats.length; i++) {
-            topHats[i] = new Hat(topRadius[i], topHeight[i]);
+    public int getNumHats(int[] topHeight, int[] topRadius, int[] bottomHeight, int[] bottomRadius) {
+        m = topHeight.length;
+        n = bottomHeight.length;
+        graph = new boolean[m][n];
+        for (int i = 0; i < topHeight.length; i++) {
+            for (int j = 0; j < bottomHeight.length; j++) {
+                graph[i][j] = bottomRadius[j] > topRadius[i] && (bottomRadius[j] * topHeight[i] > topRadius[i] * bottomRadius[j]);
+            }
         }
-        for (int i = 0; i < bottomHats.length; i++) {
-            bottomHats[i] = new Hat(bottomRadius[i], bottomHeight[i]);
-        }
-        Arrays.sort(topHats);
-        Arrays.sort(bottomHats);
+        seen = new boolean[n];
+        matchL = new int[m];
+        Arrays.fill(matchL, -1);
+        matchR = new int[n];
+        Arrays.fill(matchR, -1);
 
         int count = 0;
-        int topIndex = 0;
-        int bottomIndex = 0;
-
-        while (topIndex < topHats.length && bottomIndex < bottomHats.length) {
-
-            if (topHats[topIndex].compareTo(bottomHats[bottomIndex]) < 0) {
-                count++;
-                topIndex++;
-                bottomIndex++;
-            } else {
-                bottomIndex++;
-            }
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(seen, false);
+            if (bpm(i)) count++;
         }
         return count;
     }
 
-    private static class Hat implements Comparable {
-        int radius;
-        int height;
-
-        private Hat(int radius, int height) {
-            this.radius = radius;
-            this.height = height;
+    boolean bpm(int u) {
+        //try to match with all vertices on right side
+        for (int v = 0; v < n; v++) {
+            if (!graph[u][v]) return false;
+            if (seen[v]) continue;
+            seen[v] = true;
+            //match u and v, if v is unassigned, or if v's match on the left side can be reassigned
+            if (matchR[v] < 0 || bpm(matchR[v])) {
+                matchL[u] = v;
+                matchR[v] = u;
+                return true;
+            }
         }
-
-        double angle() {
-            return Math.atan2(radius, height);
-        }
-
-        /**
-         * returns 1 if this can go to the bottom, -1 if this can go to the top, and 0 if nothing can happen
-         */
-        public int compareTo(Object o) {
-            if (this.radius > ((Hat) o).radius && this.angle() - ((Hat) o).angle() > 0.0000001) return 1;
-            if (this.radius < ((Hat) o).radius && this.angle() - ((Hat) o).angle() < 0.0000001) return -1;
-            return 0;
-        }
+        return false;
     }
 }
