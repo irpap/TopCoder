1a946fad42b40b5bd6b5027696ae9386d97f9070
Irene <eirhnh@gmail.com>
2012-08-23 20:06:55 +0100

    a simplification

--- BadNeighbors/src/BadNeighbors.java
+++ BadNeighbors/src/BadNeighbors.java
@@ -3,35 +3,50 @@ import java.util.Arrays;
 import static java.lang.Math.max;
 
 public class BadNeighbors {
-    public int maxDonations (int[] donations) {
+    public int maxDonations(int[] donations) {
         final int n = donations.length;
+        //dp[i] stores the max donation amount we can get from 0...i while always collecting from i.
+        //the final answer would be the biggest value in the whole array cause we don't know which would be the last house of the optimal strategy
+
+        //that's correct except for the last element in the array which is skipped cause it wouldn't account for neighboring with the first
         int[] dp = new int[n];
         dp[0] = donations[0];
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n - 1; i++) {
             int maxNotNeighborIndex = i;
             for (int j = 0; j < i - 1; j++) {
-                if (dp[j] > dp[maxNotNeighborIndex]) { maxNotNeighborIndex = j; }
+                if (dp[j] > dp[maxNotNeighborIndex]) {
+                    maxNotNeighborIndex = j;
+                }
             }
             dp[i] = dp[maxNotNeighborIndex] + donations[i];
         }
-        //fix for last-first element
+
+        //that's correct except for the first element which is skipped.
         int[] dp2 = new int[n];
-        dp2[n - 1] = donations[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
+        dp2[1] = donations[1];
+        for (int i = 2; i < n; i++) {
             int maxNotNeighborIndex = i;
-            for (int j = n - 1; j > i + 1; j--) {
-                if (dp2[j] > dp2[maxNotNeighborIndex]) { maxNotNeighborIndex = j; }
+            for (int j = 1; j < i - 1; j++) {
+                if (dp2[j] > dp2[maxNotNeighborIndex]) {
+                    maxNotNeighborIndex = j;
+                }
             }
             dp2[i] = dp2[maxNotNeighborIndex] + donations[i];
         }
 
         int result = 0;
-        for (int i = 0; i < dp.length - 1; i++) { result = max(result, dp[i]); }
-        for (int i = 1; i < dp.length; i++) { result = max(result, dp2[i]); }
+        for (int i = 0; i < dp.length - 1; i++) {
+            result = max(result, dp[i]);
+        }
+        for (int i = 1; i < dp.length; i++) {
+            result = max(result, dp2[i]);
+        }
+
         return result;
     }
 
-    private void debug (final Object... a) {
+    private void debug(final Object... a) {
         System.out.println(Arrays.deepToString(a));
     }
 }
+
