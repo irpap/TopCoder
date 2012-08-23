c5a5874ea9aa4e7e0dbd8f9fe704f7579e32d4d3
Irene <eirhnh@gmail.com>
2012-08-23 20:37:37 +0100

    make the 2 loops 1 and simplify

--- BadNeighbors/src/BadNeighbors.java
+++ BadNeighbors/src/BadNeighbors.java
@@ -5,35 +5,18 @@ import static java.lang.Math.max;
 public class BadNeighbors {
     public int maxDonations(int[] donations) {
         final int n = donations.length;
-        //dp[i] stores the max donation amount we can get from 0...i while always collecting from i.
-        //the final answer would be the biggest value in the whole array cause we don't know which would be the last house of the optimal strategy
-
-        //that's correct except for the last element in the array which is skipped cause it wouldn't account for neighboring with the first
-        int[] dp = new int[n];
-        dp[0] = donations[0];
-        for (int i = 1; i < n - 1; i++) {
-            dp[i] = max((i - 2 >= 0 ? dp[i - 2] : 0) + donations[i], dp[i - 1]);
-        }
-        debug(dp);
-        //that's correct except for the first element which is skipped.
-        int[] dp2 = new int[n];
-        dp2[1] = donations[1];
-        for (int i = 2; i < n; i++) {
-            dp2[i] = max((i - 2 >= 0 ? dp2[i - 2] : 0) + donations[i], dp2[i - 1]);
+        int[][] dp = new int[2][n];
+        dp[0] = donations.clone();
+        dp[1] = donations.clone();
+        for (int i = 1, j = i + 1; i < n - 1; i++, j++) {
+            dp[0][i] = max((i - 2 >= 0 ? dp[0][i - 2] : 0) + donations[i], dp[0][i - 1]);
+            dp[1][j] = max((j - 2 >= 1 ? dp[1][j - 2] : 0) + donations[j], dp[1][j - 1]);
         }
         int result = 0;
-        for (int i = 0; i < dp.length - 1; i++) {
-            result = max(result, dp[i]);
+        for (int i = 0; i < n - 1; i++) {
+            result = max(max(result, dp[0][i]), dp[1][i + 1]);
         }
-        for (int i = 1; i < dp.length; i++) {
-            result = max(result, dp2[i]);
-        }
-
         return result;
-    }
-
-    private void debug(final Object... a) {
-        System.out.println(Arrays.deepToString(a));
     }
 }
 
