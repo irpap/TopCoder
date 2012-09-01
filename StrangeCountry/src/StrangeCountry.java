b40b063fcbaae0f46925f60b796686d3f73229dd
Irene <eirhnh@gmail.com>
2012-09-01 13:04:51 +0100

    handle the case of a single disconnected vertex

--- StrangeCountry/src/StrangeCountry.java
+++ StrangeCountry/src/StrangeCountry.java
@@ -1,4 +1,5 @@
-import static java.util.Arrays.*;
+import static java.util.Arrays.fill;
+import static java.util.Arrays.sort;
 
 public class StrangeCountry {
 
@@ -6,20 +7,64 @@ public class StrangeCountry {
     private int[] finishTime;
     private int[] entryTime;  //what time we entered each node. Also used as the "seen" array.
     private int time = 0;
+    int n;
 
     public int transform(String[] g) {
-        int n = g.length;
-        entryTime = new int[n];
-        finishTime = new int[n];
-        redundantCount = new int[n];
+        n = g.length;
+        if (thereIsDisconnectedVertex(g)) return -1;
         initialiseArrays();
+        calculateCyclesPerComponent(g);
+        int operations = mergeRedundantWithNonRedundantEdgeComponents();
+        if (singleComponentLeft()) return operations;
 
-        findComponents(g);
-        //now the redundant components are double the real value because the graph is undirected and we counted each edge twice
+        //Now there are at least two components left and they are either all Zeroes or all NonZeroes. Because if it was a 0 and a nonzero it would have been merged in the previous step
+        //if they all have zero redundant edges it's impossible to merge them
+        for (int i = 0; i < n; i++) if (redundantCount[i] == 0) return -1;
+
+        //Otherwise all components have >0 redundant edges
+        //2 components with redundant edges can be merged by doing 1 operation. For convenience I do half operation per component
+        int halfOps = 0;
+        for (int i = 0; i < n; i++) {
+            if (redundantCount[i] > 0) {
+                halfOps++;
+            }
+        }
+        return operations + (halfOps / 2);
+    }
+
+    private boolean thereIsDisconnectedVertex(String[] g) {
+        for (String s : g) if (s.indexOf('Y') < 0) return true;
+        return false;
+    }
+
+    private void dfs(String[] g, int u, int component) {
+        entryTime[u] = time++;
+        String potentialNeighbors = g[u];
+        for (int v = 0; v < potentialNeighbors.length(); v++) {
+            if (potentialNeighbors.charAt(v) == 'Y' && entryTime[v] != -1 && finishTime[v] == -1) {
+                //v is still in progress - this is a backedge, so there is a cycle
+                redundantCount[component]++;
+            }
+            if (potentialNeighbors.charAt(v) == 'Y' && entryTime[v] == -1) {
+                dfs(g, v, component);
+            }
+        }
+        finishTime[u] = time++;
+    }
+
+    private void calculateCyclesPerComponent(String[] g) {
+        int component = 0;
+        for (int i = 0; i < g.length; i++) {
+            if (entryTime[i] == -1) {
+                dfs(g, i, component++);
+            }
+        }
+        //now the redundant edges count is double than the real one because the graph is undirected and we counted each edge twice
         for (int i = 0; i < n && redundantCount[i] != -1; i++) redundantCount[i] /= 2;
+    }
 
+    private int mergeRedundantWithNonRedundantEdgeComponents() {
         int operations = 0;
-
         sort(redundantCount);
         int indexZeroRed = 0;   //index to the first zero-redundant-edges component
         while (redundantCount[indexZeroRed] == -1) indexZeroRed++;
@@ -33,58 +78,24 @@ public class StrangeCountry {
             redundantCount[indexZeroRed] = -1; //this component doesn't exist anymore, it got merged!
             indexZeroRed++;
         }
-        //if we are done, return operations made so far. We are done if there is a single component left
+        return operations;
+    }
+
+
+    private boolean singleComponentLeft() {
         int componentsLeft = 0;
         for (int i = 0; i < n; i++) if (redundantCount[i] != -1) componentsLeft++;
-        if (componentsLeft == 1) return operations;
-
-        debug(redundantCount);
-
-        //There are at least two components left and they are either all Zeroes or all NonZeroes. Because if it was a 0 and a nonzero it would have been merged in the previous step
-        //if they all have zero redundant edges it's impossible to merge them
-        for (int i = 0; i < n; i++) if (redundantCount[i] == 0) return -1;
-
-        //Otherwise they are all >0 redundant edges
-        //2 components with redundant edges can be merged by doing 1 operation. For convenience I do half operation per component
-        int halfOps = 0;
-        for (int i = 0; i < n; i++) {
-            if (redundantCount[i] > 0) {
-                halfOps++;
-            }
-        }
-        return operations + (halfOps / 2);
+        if (componentsLeft == 1) return true;
+        return false;
     }
 
     private void initialiseArrays() {
+        entryTime = new int[n];
+        finishTime = new int[n];
+        redundantCount = new int[n];
         fill(entryTime, -1);
         fill(finishTime, -1);
         fill(redundantCount, -1);
-    }
-
-    private void debug(Object... a) {
-        System.out.println(deepToString(a));
-    }
-
-    private void findComponents(String[] g) {
-        int component = 0;
-        for (int i = 0; i < g.length; i++) {
-            if (entryTime[i] == -1) dfs(g, i, component++);
-        }
-    }
-
-    private void dfs(String[] g, int u, int component) {
-        entryTime[u] = time++;
-        String neighbors = g[u];
-        for (int v = 0; v < neighbors.length(); v++) {
-            if (neighbors.charAt(v) == 'Y' && entryTime[v] != -1 && finishTime[v] == -1) {
-                //v is still in progress - this is a backedge, so there is a cycle
-                redundantCount[component]++;
-            }
-            if (neighbors.charAt(v) == 'Y' && entryTime[v] == -1) {
-                dfs(g, v, component);
-            }
-        }
-        finishTime[u] = time++;
     }
 
 
