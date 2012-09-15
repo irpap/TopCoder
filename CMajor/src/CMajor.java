40a3fbec9ac35b6dfc7d5ffe8b9af70b864d2753
Irene <eirhnh@gmail.com>
2012-09-15 14:14:28 +0100

    Memoize the play function. This aaalmost works, except example 1 which is off by 1. Hm.

--- CMajor/src/CMajor.java
+++ CMajor/src/CMajor.java
@@ -10,6 +10,7 @@ public class CMajor {
     final int keys = 12;
     int[] whiteKeys = {0, 2, 3, 5, 7, 8, 10};
     int n;
+    int[][] play;
     private String[] fragments;
 
     boolean isWhite(int k) {
@@ -18,18 +19,19 @@ public class CMajor {
     }
 
     public int getLongest(String[] fragments) {
-
         int longest = 0;
         this.fragments = fragments;
         n = fragments.length;
-        int subsets = 1 << n;
+        play = new int[keys][n];
+        for (int[] ints : play) Arrays.fill(ints, -2);
+
         for (final int startKey : whiteKeys) {
             subset:
-            for (int mask = subsets - 1; mask > 0; mask--) {
+            for (int mask = (1 << n) - 1; mask > 0; mask--) {
                 int endKey = startKey;
                 for (int i = 0; i < n; i++) {
                     if ((mask & 1 << i) != 0) {
-                        endKey = play(endKey, fragments[i]);
+                        endKey = play(endKey, i);
                         if (endKey < 0) continue subset;
                     }
                 }
@@ -45,15 +47,22 @@ public class CMajor {
         return l;
     }
 
-    private int play(int key, String fragment) {
-        if (!isWhite(key)) return -1;
-        String[] jumps = fragment.split(" ");
+    /**
+     * Ending key if we play fragment f starting from key, or -1 if we hit any black key on the way
+     */
+    private int play(int key, int f) {
+        if (play[key][f] != -2) return play[key][f];
+        String[] jumps = fragments[f].split(" ");
         for (String jump : jumps) {
             Integer intJump = Integer.valueOf(jump);
             key = (key + intJump) % keys;
             if (key < 0) key = keys + key;
-            if (!isWhite(key)) return -1;
+            if (!isWhite(key)) {
+                play[key][f] = -1;
+                return -1;
+            }
         }
+        play[key][f] = key;
         return key;
     }
 }
